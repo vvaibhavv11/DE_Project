16bb5226b7030809383867a92f7ef9104aeaa12c
vvaibhavv11 <vvaibhavv3434@skiff.com>
2024-11-12 22:55:53 +0530

    noob

--- server.js
+++ server.js
@@ -135,4 +135,9 @@ app.post('/send-email', async (req, res) => {
 	}
 });
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 module.exports = app;
